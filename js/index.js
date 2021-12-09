1e8a434b113949be2df89e93da53c9f5c49e28ea
Max D <max.moderation@gmail.com>
2021-12-09 20:57:35 +0200

    relayouting, add js struct

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const addTaskBtn = document.getElementsByClassName('.add-task-btn');
+const taskDesc = document.getElementsByClassName('.task-description');
+const todoList = document.getElementsByClassName('.todo-list');
+
+
