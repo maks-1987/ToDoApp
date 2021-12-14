fbf2c284a224d792816a4fa7a753fc7ea3fda547
Max D <max.moderation@gmail.com>
2021-12-14 21:03:37 +0200

    edit task-save in localStorage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-const addTaskBtn = document.getElementsByClassName('.add-task-btn');
-const taskDesc = document.getElementsByClassName('.task-description');
+const taskBtn = document.getElementById('add-task-btn');
+const taskDesc = document.getElementById('task-description');
 const todoList = document.getElementsByClassName('.todo-list');
 
+let tasks;
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
+// samples obj if click btn
+function NewTask(description) {
+  this.description = description;
+  this.isFinished = false;
+}
+
+let updateStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+taskBtn.addEventListener('click', () => {
+  tasks.push(new NewTask(taskDesc.value));
+  updateStorage();
+  console.log(tasks);
+});
